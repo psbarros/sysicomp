c0d08d65566ca1acf3809044b7b37dee9622eb7e
David Fernandes <david@icomp.ufam.edu.br>
2017-08-21 08:15:12 -0400

    Adicionando o layout em .gitignore

--- apagar/adicaoDeRedundaciaNaCautela.sql
+++
@@ -1,6 +0,0 @@
-UPDATE j17_cautela
-
-SET idEquipamento = cast(Equipamento as  INT)
-
-WHERE
-	idEquipamento != cast(Equipamento as  INT);
--- apagar/patchesDoBd.sql
+++
@@ -1,7 +0,0 @@
-
--- Transforma a chave primaria em AutoIncremental
-ALTER TABLE `j17_equipamento` CHANGE `idEquipamento` `idEquipamento` INT(11) NOT NULL AUTO_INCREMENT;
-
-
--- Transforma a chave primaria em AutoIncremental
-ALTER TABLE `j17_cautela_avulsa` CHANGE `idCautelaAvulsa` `idCautelaAvulsa` INT(11) NOT NULL AUTO_INCREMENT;
